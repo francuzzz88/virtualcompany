011f8acb3ab4c467e4897fb3b4a2112d376f15c1
Evgeniy <francuzzz_@mail.ru>
2022-05-11 13:02:05 +0300

    init project

---
+++ src/main/java/ru/petrov/virtualcompany/VirtualCompanyApplication.java
@@ -0,0 +1,13 @@
+package ru.petrov.virtualcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/petrov/virtualcompany/VirtualCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petrov.virtualcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
